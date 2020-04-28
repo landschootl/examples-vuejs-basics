fe66686d850fd540cad1f406d72c0fdfbbee308d
landchootl <ludovic.landschoot.partner@decathlon.com>
2020-04-28 20:11:18 +0200

    feat (properties): init component

--- js/base.js
+++ js/base.js
@@ -1,6 +1,31 @@
 new Vue({
   el:'#app',
+  components: {
+    // MonComposant
+  },
   data: {
-    message: 'Prêt ?!'
-  }
+    message: 'Pictime'
+  },
+  methods: {
+    changeMessage() {
+      this.message = 'les gens';
+    }
+  },
+  watch: {
+    message() {
+      console.log('mon message a changer');
+    }
+  },
+  computed: {
+    fullMessage() {
+      return `Salut ${this.message}`;
+    }
+  },
+  template: `
+        <div>
+          <p>{{fullMessage}}</p>
+<!--          <button @click="changeMessage()">changer le message</button>-->
+        </div>
+      `
+
 });
